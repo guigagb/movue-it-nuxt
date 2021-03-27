61ee9e263e629f8fdbef614b2d263a3440d24ab7
guigagb <bsbtrindade@gmail.com>
2021-03-27 15:16:19 -0300

    feat: create components CompletedChallenges and Profile

---
+++ components/molecules/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="flex items-center">
+		<img src="https://github.com/guigagb.png" alt="Guilherme" class="mr-6 w-20 h20 rounded-full" />
+		<div class="flex flex-col w-full">
+			<strong class="text-title text-2xl">Guilherme</strong>
+			<p class="flex items-center">
+				<img src="icons/level.svg" alt="level" class="mr-2" />
+				<span class="text-base">Level {{ level }}</span>
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { mapState } from "vuex";
+
+export default Vue.extend({
+	computed: {
+		...mapState("Challenges", ["level"]),
+	},
+});
+</script>
